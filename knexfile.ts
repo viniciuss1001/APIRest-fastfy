1fb17e3c48bc179eeae066464cba7992fc1e9207
vinicius2 <viniciussantosfrota10012017@gmail.com>
2024-03-18 20:57:00 -0300

    knex conffiguration

---
+++ knexfile.ts
@@ -0,0 +1,6 @@
+import {configKnex} from './src/database'
+
+export default configKnex
+
+//npm run knex -- h 
+//command to acess the options of command from knex
--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,11 @@
-export const knex = require('knex')({
+import { knex as SetupKnex } from 'knex'
+
+export const configKnex = {
   client: 'sqlite3',
   connection: {
     filename: './temp/app.db',
   },
-})
+  useNullAsDefault: true,
+}
+
+export const knex = SetupKnex(configKnex)
